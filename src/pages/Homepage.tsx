11483fc3b574f76a1fe6ec931a2db6bfa172f7d0
Ye Khaing Win <yekhaingwinn@gmail.com>
2024-09-10 18:31:05 +0800

    add tailwindcss

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,18 +1,19 @@
-import styles from "./Homepage.module.css";
+// import styles from "./Homepage.module.css";
 
 export default function Homepage() {
   return (
-    <main className={styles.homepage}>
-      <section>
-        <h1>
+    <main className="h-hero m-10 py-10 px-20 bg-cover bg-center bg-hero">
+      <section className="flex flex-col h-[85%] items-center justify-center gap-10 text-center">
+        <h1 className="text-7xl leading-[1.3]">
           You travel the world.
           <br />
           WorldWise keeps track of your adventures.
         </h1>
-        <h2>
-          A world map that tracks your footsteps into every city you can think
-          of. Never forget your wonderful experiences, and show your friends how
-          you have wandered the world.
+        <h2 className="w-11/12 text-3xl text-light-1 mb-10">
+          A world map that tracks your footsteps into every
+          city you can think of. Never forget your wonderful
+          experiences, and show your friends how you have
+          wandered the world.
         </h2>
       </section>
     </main>
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      height: {
+        hero: "calc(100vh - 5rem)",
+      },
+      backgroundImage: {
+        hero: () =>
+          'linear-gradient(rgba(36, 42, 46, 0.8),rgba(36, 42, 46, 0.8)),url("../bg.jpg")',
+      },
+      fontFamily: {
+        manrope: ["Manrope", "sans-serif"],
+      },
+      colors: {
+        "brand-1": "#ffb545",
+        "brand-2": "#00c46a",
+        "dark-0": "#242a2e",
+        "dark-1": "#2d3439",
+        "dark-2": "#42484d",
+        "light-1": "#aaa",
+        "light-2": "#ececec",
+        "light-3": "#d6dee0",
+      },
+    },
+  },
+  plugins: [],
+};
